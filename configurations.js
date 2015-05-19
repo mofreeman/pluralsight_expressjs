f5fa4f736d62d523da76ad8fe7bf56f5acb8c757
mofreeman <mofreeman@gmail.com>
2015-05-19 03:52:55 +0000

    updated configuraiton code to work with express 4+

--- configurations.js
+++ configurations.js
@@ -2,20 +2,40 @@ var express = require('express');
 var app = express();
 
 //Define NODE_ENV=produciton prior to launching node - default is development
-app.configure('production', function() {
-   //middleware
-   app.set('title', 'CRM Applicaiton');
-});
 
+//Old Way
+//app.configure('production', function() {
+//   //middleware
+//   app.set('title', 'CRM Applicaiton');
+//});
 
-app.configure('development', function() {
-   //middleware
-   
-   app.set('title', 'CRM Applicaiton - Development');
-});
+//app.configure('development', function() {
+//   //middleware
+//   app.set('title', 'CRM Applicaiton - Development');
+//});
+
+//sets public directory - can use /sample.html
+//app.configure( function() {
+//    app.use(express.logger('dev'));
+//    app.use(express.faicon());
+//    app.use(express.static(__dirname + '/public'))
+//});;
+
+// all environments
+app.use(express.static(__dirname + '/public'))
+
+var env = process.env.NODE_ENV || 'development';
+// development only
+if (env == 'development') {
+    app.set('title', 'CRM Applicaiton - Development');
+}
+// production only
+if (env == 'production') {
+    app.set('title', 'CRM Applicaiton');
+}
+
 
 app.get('/', function(req,res) {
-    
     res.send('Value of title is ' + app.get('title'));
 });
 
